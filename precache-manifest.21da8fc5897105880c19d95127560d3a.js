732a1740a9303e58c16c0c7e295edfc08a7a49d6
4happy-studio <4happy.studio@gmail.com>
2020-03-27 22:44:59 +0700

    Updates

---
+++ precache-manifest.21da8fc5897105880c19d95127560d3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1411cd4d417d0f39e7fac75655ed7c0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88200a0dfec7e68f19ea",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "53021b572a0c2a94abb2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88200a0dfec7e68f19ea",
+    "url": "/static/js/2.18198b6b.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.18198b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53021b572a0c2a94abb2",
+    "url": "/static/js/main.91ceb716.chunk.js"
+  },
+  {
+    "revision": "5054aa2e6c5709ad8041",
+    "url": "/static/js/runtime-main.f9a35a1f.js"
+  }
+]);
